7f07d820ad2dc5ca202a5c3006171e2c7bfed4db
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-05-19 09:10:16 +0100

    Updates

--- cryptsy-compute-prices.py
+++ cryptsy-compute-prices.py
@@ -1,10 +1,9 @@
 import urllib2
-import simplejson
-from datetime import date
-from datetime import datetime
+import datetime
 
 import numpy
 
+import simplejson
 from pymongo import MongoClient
 
 
@@ -25,6 +24,8 @@ def main():
     marketNames = [market for market in marketDetails]
     # marketNames = filter(lambda x: float(x.lasttradeprice) != 0.0, marketDetails)
 
+    filteredMarketNames = filter(lambda x: float(marketDetails[x]['lasttradeprice']) < 0.001, marketNames)
+
     client = MongoClient()
 
     cryptsyDb = client.cryptsy_database
@@ -33,12 +34,18 @@ def main():
 
     myMarkets = []
 
-    # yesterday = date.fromordinal(date.today().toordinal() - 1)
-    yesterday = date.fromordinal(date.today().toordinal())
+    # timeStart = date.fromordinal(date.today().toordinal() - 1)
+    # timeStart = date.fromordinal(date.today().toordinal())
+    now = datetime.datetime.now()
+    # timeStart = now.replace(day=now.day - 1)
+    timeStart = now.replace(hour=now.hour - 1)
 
-    for marketName in marketNames:
+    print "time: {}".format(timeStart)
+
+    for marketName in filteredMarketNames:
+
         sxcBtcPrices = marketsCollection.find(
-            {"name": marketName, "lasttradetime": {"$gt": yesterday.strftime("%Y-%m-%d")}})
+            {"name": marketName, "lasttradetime": {"$gt": timeStart.strftime("%Y-%m-%d")}})
 
         if sxcBtcPrices.count() == 0:
             # print "Continuing, marketName: {}".format(marketName)
@@ -49,28 +56,40 @@ def main():
         myMarkets.append(MyMarket(marketName=marketName,
                                   min=min(prices),
                                   max=max(prices),
+                                  average=numpy.average(prices),
                                   std=numpy.std(prices)))
 
+    # sortedMarkets = sorted(myMarkets, key=lambda x: x.rate, reverse=True)
+    # sortedMarkets = sorted(myMarkets, key=lambda x: x.std, reverse=True)
     sortedMarkets = sorted(myMarkets, key=lambda x: x.rate, reverse=True)
 
-    for index in range(min(10, len(sortedMarkets))):
+    # sortedFilteredMarkets = filter(lambda x: x.average > 0.0001, sortedMarkets)
+
+    for index in range(min(20, len(sortedMarkets))):
         print sortedMarkets[index]
 
 
 class MyMarket:
-    def __init__(self, marketName, min, max, std):
+    def __init__(self, marketName, min, max, average, std):
         self.marketName = marketName
         self.min = min
         self.max = max
+        self.average = average
         self.std = std
-        self.rate = abs((float(max) - float(min)) / float(min) * 100)
+        self.rate = abs((float(average + std) - float(average - std)) / float(average - std) * 100)
 
     def __str__(self):
-        return "Name: {}, min: {}, max: {}, std: {}, rate: {}".format(self.marketName,
-                                                                      self.min,
-                                                                      self.max,
-                                                                      self.std,
-                                                                      self.rate)
+        return "Name: {}, min: {}, max: {}, avg: {}, std: {}, standardRate: {}, suggestedMinPrice {}, suggestedMaxPrice {}, possibleEarningPerUnit {}".format(
+            self.marketName,
+            self.min,
+            self.max,
+            self.std,
+            self.average,
+            self.rate,
+            self.average - self.std,
+            self.average + self.std,
+            self.std * 2
+        )
 
 
 if __name__ == "__main__":
