2660cb8f00db84ed486d8ef251787f4887027bdb
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-07-04 11:17:52 +0100

    Updated Sampler

--- cryptsy-market-to-mongo.py
+++ cryptsy-market-to-mongo.py
@@ -7,14 +7,31 @@ from pymongo.mongo_client import MongoClient
 
 
 class Market:
-    def __init__(self, marketid, name, lasttradeprice, lasttradetime):
+    def __init__(self, marketid, tradeid, name, lasttradeprice, lasttradetime):
         self.marketid = marketid
+        self.tradeid = tradeid
         self.name = name
         self.lasttradeprice = lasttradeprice
         self.lasttradetime = lasttradetime
 
+    def __str__(self):
+        return "marketid: {}, tradeid: {}, name: {}, lasttradeprice:{}, lasttradetime: {}".format(self.marketid,
+                                                                                                  self.tradeid,
+                                                                                                  self.name,
+                                                                                                  self.lasttradeprice,
+                                                                                                  self.lasttradetime)
+
 
 def main():
+
+    client = MongoClient()
+    cryptsyDb = client.cryptsy_database
+    marketsCollection = cryptsyDb.markets_collection
+    result = marketsCollection.find().sort('tradeid', -1).limit(1)
+    maxtradeid = 0
+    if result.count() > 0:
+        maxtradeid = result[0]['tradeid']
+
     cryptsyApi = "http://pubapi.cryptsy.com/api.php?method=marketdatav2"
 
     cryptsyMarketDataResponse = urllib2.urlopen(cryptsyApi)
@@ -28,18 +45,17 @@ def main():
         allMarkets = []
 
         for market in markets:
-            allMarkets.append(Market(marketid=markets[market]["marketid"],
-                                     name=markets[market]["label"],
-                                     lasttradeprice=markets[market]["lasttradeprice"],
-                                     lasttradetime=markets[market]["lasttradetime"]))
+            if 'recenttrades' in markets[market] and markets[market]['recenttrades'] is not None:
+                for recenttrade in markets[market]['recenttrades']:
+                    allMarkets.append(Market(marketid=markets[market]["marketid"],
+                                             tradeid=recenttrade['id'],
+                                             name=markets[market]["label"],
+                                             lasttradeprice=recenttrade['price'],
+                                             lasttradetime=recenttrade['time']))
 
-        activeMarkets = filter(lambda x: x.lasttradeprice is not None and float(x.lasttradeprice) != 0.0, allMarkets)
-
-        client = MongoClient()
-
-        cryptsyDb = client.cryptsy_database
-
-        marketsCollection = cryptsyDb.markets_collection
+        activeMarkets = filter(
+            lambda x: x.lasttradeprice is not None and float(x.lasttradeprice) != 0.0 and x.tradeid > maxtradeid,
+            allMarkets)
 
         for market in activeMarkets:
             marketsCollection.insert(market.__dict__)
