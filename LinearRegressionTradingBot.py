fdb38e6267e3d82dbc988191d94daf52ded65180
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-07-13 15:22:37 +0100

    Increased from 2 to 3 the numeber of days for checking markets historical perfofmances

--- LinearRegressionTradingBot.py
+++ LinearRegressionTradingBot.py
@@ -97,7 +97,7 @@ def investBTC(btcBalance, activeMarkets, marketData):
     sortedMarketTrends = filter(lambda x: x.m != 0.0 and x.avg >= 0.000001 and x.std > 4 * (x.avg * FEE),
                                 sorted(marketTrends, key=lambda x: abs(0.0 - x.m)))
 
-    bestPerformingMarkets = cryptsyClient.getBestPerformingMarketsInTheLast(2)[:4]
+    bestPerformingMarkets = cryptsyClient.getBestPerformingMarketsInTheLast(3)[:4]
 
     worstPerformingMarkets = cryptsyClient.getWorstPerformingMarketsInTheLast(3)
 
