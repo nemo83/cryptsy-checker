f7f2dd5ee78b0ae739306f6a1219abd3be3ea1dd
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-07-25 14:24:08 +0100

    increase multiplier for best performing market

--- LinearRegressionTradingBot.py
+++ LinearRegressionTradingBot.py
@@ -95,7 +95,7 @@ def investBTC(btcBalance, activeMarkets, markets):
         if marketTrend.marketId in userMarketIds:
             desiredAmountToInvest = BASE_STAKE
         elif marketTrend.marketId in bestPerformingMarkets[:1]:
-            desiredAmountToInvest = BASE_STAKE * 8
+            desiredAmountToInvest = BASE_STAKE * 12
         elif marketTrend.marketId in bestPerformingMarkets[1:2]:
             desiredAmountToInvest = BASE_STAKE * 6
         elif marketTrend.marketId in bestPerformingMarkets[2:3]:
