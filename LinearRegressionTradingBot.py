63f0f28e3f973f85b61274d88a35aaa839987173
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-07-27 17:05:43 +0100

    Removed limit to best markets.

--- LinearRegressionTradingBot.py
+++ LinearRegressionTradingBot.py
@@ -77,7 +77,7 @@ def investBTC(btcBalance, activeMarkets, markets):
     sortedMarketTrends = filter(lambda x: x.m != 0.0 and x.avg >= 0.0000005 and x.std > 4 * (x.avg * FEE),
                                 sorted(marketTrends, key=lambda x: abs(0.0 - x.m)))
 
-    bestPerformingMarkets = cryptsyClient.getBestPerformingMarketsInTheLastFeeIncluded(3)[:6]
+    bestPerformingMarkets = cryptsyClient.getBestPerformingMarketsInTheLastFeeIncluded(3)
 
     worstPerformingMarkets = cryptsyClient.getWorstPerformingMarketsInTheLastFeeIncluded(3)
 
