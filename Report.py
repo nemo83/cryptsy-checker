617820c5d8d460e63e03c044ae1ce72e4b33dbd9
Giovanni Gargiulo <ggargiulo@groupon.com>
2014-08-08 14:55:08 +0100

    Updated Reports

--- Report.py
+++ Report.py
@@ -30,6 +30,9 @@ def main(argv):
 
     start_time = toCryptsyServerTime(datetime.utcnow() - timedelta(days=int(days)))
 
+    total_buy_best = 0.0
+    total_sell_best = 0.0
+    total_fee_best = 0.0
     print "Best markets:"
     mongotradeStats = cryptsy_mongo.getAllTradesFrom(start_time)
     filteredTradeStats = filter(
@@ -39,6 +42,9 @@ def main(argv):
                               reverse=True)
 
     for tradeStat in sortedTradeStats:
+        total_buy_best += float(mongotradeStats[tradeStat]['Buy'])
+        total_sell_best += float(mongotradeStats[tradeStat]['Sell'])
+        total_fee_best += float(mongotradeStats[tradeStat]['Fee'])
         print "MarketId: {}, Sell: {}, Buy: {}, Earn: {}".format(tradeStat,
                                                                  mongotradeStats[tradeStat]['Sell'],
                                                                  mongotradeStats[tradeStat]['Buy'],
@@ -46,6 +52,13 @@ def main(argv):
                                                                  mongotradeStats[tradeStat][
                                                                      'Buy'])
 
+    print "Best markets total: buy: {}, sell: {}, fee:{} - earnings: {}".format(total_buy_best, total_sell_best,
+                                                                                total_fee_best,
+                                                                                total_sell_best - total_buy_best - total_fee_best)
+
+    total_buy_worst = 0.0
+    total_sell_worst = 0.0
+    total_fee_worst = 0.0
     print "Worst markets:"
     mongotradeStats = cryptsy_mongo.getAllTradesFrom(start_time)
     filteredTradeStats = filter(
@@ -54,6 +67,9 @@ def main(argv):
     sortedTradeStats = sorted(filteredTradeStats, key=lambda x: mongotradeStats[x]['Sell'] - mongotradeStats[x]['Buy'])
 
     for tradeStat in sortedTradeStats:
+        total_buy_worst += float(mongotradeStats[tradeStat]['Buy'])
+        total_sell_worst += float(mongotradeStats[tradeStat]['Sell'])
+        total_fee_worst += float(mongotradeStats[tradeStat]['Fee'])
         print "MarketId: {}, Sell: {}, Buy: {}, Fee: {}, Earn: {}".format(tradeStat,
                                                                           mongotradeStats[tradeStat]['Sell'],
                                                                           mongotradeStats[tradeStat]['Buy'],
@@ -61,6 +77,13 @@ def main(argv):
                                                                           mongotradeStats[tradeStat]['Sell'] -
                                                                           mongotradeStats[tradeStat]['Buy'],
                                                                           mongotradeStats[tradeStat]['Fee'])
+
+    print "Worst markets total: buy: {}, sell: {}, fee:{} - earnings: {}".format(total_buy_worst, total_sell_worst,
+                                                                                 total_fee_worst,
+                                                                                 total_sell_worst - total_buy_worst - total_fee_worst)
+
+    print "Total stats: total earning: {}".format(
+        (total_sell_best + total_sell_worst) - (total_buy_best + total_buy_worst + total_fee_best + total_fee_worst))
 
 
 def getEnv(argv):
